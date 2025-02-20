5b97da8cca15e437d3487b23c757a01e2eb52fdf
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2025-02-20 13:34:00 +0200

    add not-found component with 404 error page styling and routing

--- src/app/Components/not-found/not-found.component.ts
+++ src/app/Components/not-found/not-found.component.ts
@@ -1,9 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterModule
+  ],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
